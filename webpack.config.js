3aeb03d2d09e0d4b20351a6d4815d766ef30dcf2
liuwanyong2017 <2674706698@qq.com>
2020-04-27 13:02:31 +0800

    实现scss的加载，打包，插入成style。用的node-sass,sass-loader，有梯子，随意下载！

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,45 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",//入口
     output: {
         filename: "bundle.[hash:5].js",  //文件名，哈希，避免覆盖，或者缓存问题
         path: path.resolve(__dirname, "./dist")  //路径绝对路径
     },
-    mode:'development', //生产模式，开发模式 production
+    mode: "development", //生产模式，开发模式 production
     devServer: {  //开发服务器配置
-        port:3000,  //端口
-        progress:true,//进度条
-        contentBase:'./dist', //打开的目录
-        compress:true ,//Gzip压缩
+        port: 3000,  //端口
+        progress: true,//进度条
+        contentBase: "./dist", //打开的目录
+        compress: true,//Gzip压缩
     },
-    plugins:[  //插件
+    plugins: [  //插件
         new HtmlWebpackPlugin({
-            template: './src/index.html',  //html地址
-            filename: 'index.html' ,  //输出文件名
-            minify:{  //压缩html
-                removeAttributeQuotes:true  , //删除html中的双引号
-                collapseWhitespace:true, //折叠空行html
-                hash:true   , //缓存问题
+            template: "./src/index.html",  //html地址
+            filename: "index.html",  //输出文件名
+            minify: {  //压缩html
+                removeAttributeQuotes: true, //删除html中的双引号
+                collapseWhitespace: true, //折叠空行html
+                hash: true, //缓存问题
             }
         })
     ],
-    module:{//模块
+    module: {//模块
         rules: [  //规则,匹配文件，处理文件
             {
                 //css-loader 解析css文件引入css文件的，style-loader把css文件数据插入到html中
                 //loader 有顺序的,默认从右向左，从下到上执行！
                 //更多的参数配置写法：{loader:'style-loader',options:{**:**}}
-                test:/\.css$/,use:['style-loader','css-loader'],
+                // less , sass stylus node-sass sass-loader ,stylus stylus-loader
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                        //options: {insert: "top"}  //控制打包后的css标签放在html内的原本有的style标签前面，覆盖用的
+                    },
+                    "css-loader",   //css文件@import css文件
+                    'sass-loader'  //scss转为css
+                ]
             }
         ]
     }
